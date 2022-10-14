4fa876f6d2dd0f7466abe922304d2b822ac9a3a2
Ivan M <slade91@mail.ru>
2022-10-14 17:13:46 +0300

    first

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет*/
+
+Console.WriteLine("Введите  число:");
+int day = int.Parse(Console.ReadLine());
+int weekend = 6;
+int max = 8;
+
+if (day >= weekend && day >= max)
+    Console.WriteLine(" Вы ввели неверное число!");
+
+else if (day < weekend)
+
+    Console.WriteLine(" Будний день");
+
+else
+    Console.WriteLine(" Выходной день");
